db2ec81fbf3f155d139692bd0cfecf131e2010a8
Come1LLF00 <rayveleaveme34@inbox.ru>
2020-10-18 17:30:44 +0300

    wrote DB interaction, correct configs need to add dataSource and correct jsf configs

--- src/main/java/beans/ResultBean.java
+++ src/main/java/beans/ResultBean.java
@@ -8,23 +8,34 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-@ManagedBean(name = "resultBean", eager = true)
-@ApplicationScoped
 public class ResultBean implements Serializable {
     private static final long  serialVersionUID = 1L;
-    private Entry entry = new Entry();
-    public List<Entry> resultyEntry = new ArrayList<Entry>();
+
+    private Double x;
+    private Double y;
+    private Double r;
+    private boolean hit = false;
+    private long time;
+
+    public Double getX() { return x; }
+    public Double getY() { return y; }
+    public Double getR() { return r; }
+    public boolean isHit() { return hit; }
+    public long getTime() { return time; }
+
+    public void setX(Double x) { this.x = x; }
+    public void setY(Double y) { this.y = y; }
+    public void setR(Double r) { this.r = r; }
+    public void setHit(boolean hit) { this.hit = hit; }
+    public void setTime(long time) { this.time = time; }
 
     public ResultBean() { }
 
-    public Entry getEntry() {
-        return entry;
-    }
-    public void setEntry(Entry entry) {
-        this.entry = entry;
+    public List<ResultsEntityManager> getResultsListFromDB() {
+        return DBOperator.getAllResultsDetails();
     }
 
-    public boolean isHit() {
-        return Check.isHit(entry);
+    public String addNewResult(ResultBean added) {
+        return DBOperator.createNewResults(added.x, added.y, added.r, added.hit ? "yes" : "no", added.time);
     }
 }
